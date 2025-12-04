af9dfb1c6df485d50ea42a270f7bffd085560b84
ryotafuwa <ryota.fuwa.1428+github@gmail.com>
2025-12-04 22:24:42 +0900

    cleanup

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ provider "github" {
   owner = "rfuwa14"
 }
 
-module "repository" {
-  source = "./modules/repository"
-
-  repository_name        = "my-test-repo"
-  repository_description = "my test repo (will be deleted soon)"
-  auto_init              = true
-}
+# module "repository" {
+#   source = "./modules/repository"
+#
+#   repository_name        = "my-test-repo"
+#   repository_description = "my test repo (will be deleted soon)"
+#   auto_init              = true
+# }
 
