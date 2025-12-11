f5ede7ce8baf63d031246653631d85d44028624b
rfuwa <ryota.fuwa.1428+github@gmail.com>
2025-12-12 08:36:35 +0900

    added semantic-release for tf modules

---
+++ modules/repository/.releaserc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: "../.releaserc.base.cjs",
+  tagFormat: "repository@${version}",
+  plugins: [
+    [
+      "@semantic-release/git",
+      {
+        assets: ["modules/repository/CHANGELOG.md"],
+        message:
+          "chore(release): repository@${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
+
